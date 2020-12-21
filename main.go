1bde6467e371b714b503e59a320efc4cb73dceed
Tejus Tiwari <tejustiwari@gmail.com>
2020-12-22 00:58:17 +0530

    Update main.go

--- main.go
+++ main.go
@@ -38,20 +38,20 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(result)
 }
 
-// 2.   GET: /users/<id here> to Get a user using id
+// 2.   GET: /users/<id here> to Get a user using id. This is a generic Route for all users i.e. id is a variable.
 func getUser(w http.ResponseWriter, r *http.Request) {
 	// set header.
 	w.Header().Set("Content-Type", "application/json")
 
 	var user schema.User
 
-	id := strings.TrimPrefix(r.URL.Path, "/users/")
+	id := strings.TrimPrefix(r.URL.Path, "/users/") // Extracting user id from the URL by performing expression/pattern matching
 	// OR
 	// re := regexp.MustCompile("/users/([!-z]+)")
-	// id := re.FindStringSubmatch(r.URL.Path)[1]
+	// id := re.FindStringSubmatch(r.URL.Path)[1] 
 
 	filter := bson.M{"id": id}
-	// fmt.Println(id)
+	
 	err := users.FindOne(context.TODO(), filter).Decode(&user)
 
 	if err != nil {
@@ -70,7 +70,6 @@ func createContact(w http.ResponseWriter, r *http.Request) {
 	// we decode our body request params
 	_ = json.NewDecoder(r.Body).Decode(&contact)
 
-	// insert our book model.
 	result, err := contacts.InsertOne(context.TODO(), contact)
 
 	if err != nil {
@@ -98,15 +97,15 @@ func getContacts(w http.ResponseWriter, r *http.Request) {
 
 	cur, err := contacts.Find(
 		context.TODO(),
-		bson.D{
-			{"$or",
+		bson.D{    // Filtering out the contacts. ( This filter is made by combining multiple small filters using "AND" and "OR" operations. )
+			{"$or",           // filter for getting values with -> either "useridone == userID" or "useridtwo == userID"
 				bson.A{
 					bson.D{{"useridone", userID}},
 					bson.D{{"useridtwo", userID}},
 				},
 			},
-			{"timeofcontact", bson.M{"$gte": fourteenDaysBeforeTimestamp}},
-			{"timeofcontact", bson.M{"$lte": infectionTimestamp}},
+			{"timeofcontact", bson.M{"$gte": fourteenDaysBeforeTimestamp}}, // filter to get time >= fourteenDaysBeforeTimestamp
+			{"timeofcontact", bson.M{"$lte": infectionTimestamp}}, // filter to get time <= finfectionTimestamp
 		},
 	)
 
@@ -143,7 +142,7 @@ func getContacts(w http.ResponseWriter, r *http.Request) {
 func main() {
 	// Arrange the routes
 	http.HandleFunc("/users", createUser)
-	http.HandleFunc("/", getUser) // Generic Route, regular exp matching is done in getUser Handler
+	http.HandleFunc("/", getUser) // Generic Route (Eg. URL: "/userid_1"), regular exp matching is done in getUser Handler to get the userID of the desired user
 	http.HandleFunc("/contacts", createContact)
 	http.HandleFunc("/contacts/", getContacts)
 
